3ab700237f4b5b2e62fb6a8795d77a097aae4b33
o.smirnov <oleg.smirnov@firstlinesoftware.com>
2019-07-03 17:54:06 +0300

    Task #55 Добавить контроллер принимающий параметры объектов для передачи их библиотеки .Net RayTracer

---
+++ RayTracingMVC/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RayTracingLib;
+using RayTracingMVC.Models;
+
+namespace RayTracingMVC.Controllers
+{
+    public class RenderController : ApiController
+    {
+        [HttpPost]
+        public int RenderImage(RenderImageRequest request)
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ RayTracingMVC/Models/RenderImageRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RayTracingLib;
+
+namespace RayTracingMVC.Models
+{
+    public class RenderImageRequest
+    {
+        public Geometry.Geometry.Vec3f Origin { get; set; }
+        public List<Sphere> Spheres { get; set; }
+        public List<Light> Lights { get; set; }
+    }
+}
